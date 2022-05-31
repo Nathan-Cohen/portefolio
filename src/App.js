870e10aad4fc23da67d1ad54bcc256de112846bc
Nathan-Cohen <manathane.co@gmail.com>
2022-06-01 01:39:53 +0200

    portfolio v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Projects from './Containers/Projects/Projects';
+import Contact from './Containers/Contact/Contact'
+import Experiences from './Containers/Experiences/Experiences';
+import Home from './Containers/Home/Home';
+import NavBar from './Components/Navbar/Navbar';
+import Formations from './Containers/Formations/Formations'
+import NavFooter from './Components/NavFooter/NavFooter';
+import "./font-awesome-4.7.0/css/font-awesome.min.css";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <NavBar />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects/:slug' element={<Projects />} />
+          <Route path='/experiences' element={<Experiences />} />
+          <Route path='/formations' element={<Formations />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+
+        <NavFooter />
     </div>
   );
 }
